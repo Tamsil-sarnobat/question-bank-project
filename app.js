edd0ddd8f27e622068f1785a2b35536e250ac5f0
Iqra Jambharkar <iqrajambharkar@gmail.com>
2025-07-13 01:43:32 +0530

    create semester page

--- app.js
+++ app.js
@@ -3,39 +3,40 @@ const mongoose = require("mongoose");
 const app = express();
 const path = require("path");
 const ejsMate = require("ejs-mate");
+const subjects = require("./init/subjects.js")
 
 const mongoLink = "mongodb://127.0.0.1:27017/QBProject";
 
-
 main()
-.then(()=>{
-   console.log("Connected to database");
-})
-.catch(err => console.log(err));
-
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch((err) => console.log(err));
 
 async function main() {
   await mongoose.connect(mongoLink);
 }
 
-app.set("view engine","ejs");
-app.set("views",path.join(__dirname,"views"));
-app.use(express.static(path.join(__dirname,"/public")));
-app.use(express.urlencoded({extended:true}));
-app.engine("ejs",ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.urlencoded({ extended: true }));
+app.engine("ejs", ejsMate);
 
 
 
-
-app.get("/",(req,res)=>{
-    res.send("Working On The Root!");
-})
-
-app.get("/semester/:id", (req,res) => {
-  const semId = req.params.id;
-  res.render("semester.ejs", {semId});
+app.get("/", (req, res) => {
+  res.send("Working On The Root!");
 });
 
-app.listen(8080,()=>{
-    console.log("listening on the port 8080");
+app.get("/semester/:id", (req, res) => {
+  const semId = parseInt(req.params.id);
+  const filteredSubjects = subjects.filter((sub) => {
+    return sub.semester === semId
+  });
+  res.render("semester.ejs", { semId, subjects: filteredSubjects });
+});
+
+app.listen(8080, () => {
+  console.log("listening on the port 8080");
 });
---
+++ init/subjects.js
@@ -0,0 +1,164 @@
+//Temporary Subjects Data
+
+const subjects = [
+  //sem1
+  {
+    name: "Subject 1",
+    semester: 1,
+  },
+
+  {
+    name: "Subject 2",
+    semester: 1,
+  },
+
+  {
+    name: "Subject 3",
+    semester: 1,
+  },
+
+  { 
+  name: "Subject 4", 
+  semester: 1 
+ },
+
+ { 
+  name: "Subject 5", 
+  semester: 1 
+ },
+
+ //sem2
+
+ { 
+  name: "Subject 1", 
+  semester: 2 
+ },
+
+ { 
+  name: "Subject 2", 
+  semester: 2 
+ },
+
+ { 
+  name: "Subject 3", 
+  semester: 2
+ },
+
+ { 
+  name: "Subject 4", 
+  semester: 2
+ },
+
+ { 
+  name: "Subject 5", 
+  semester: 2
+ },
+
+ //sem3
+
+ { 
+  name: "Subject 1", 
+  semester: 1 
+ },
+
+ { 
+  name: "Subject 2", 
+  semester: 1 
+ },
+
+ { 
+  name: "Subject 3", 
+  semester: 1 
+ },
+
+ { 
+  name: "Subject 4", 
+  semester: 1 
+ },
+
+ { 
+  name: "Subject 5", 
+  semester: 1 
+ },
+
+ //sem4
+
+ { 
+  name: "Subject 1", 
+  semester: 4 
+ },
+
+ { 
+  name: "Subject 2", 
+  semester: 4 
+ },
+
+ { 
+  name: "Subject 3", 
+  semester: 4
+ },
+
+ { 
+  name: "Subject 4", 
+  semester: 4 
+ },
+
+ { 
+  name: "Subject 5", 
+  semester: 4 
+ },
+
+ //sem5
+ { 
+  name: "Subject 1", 
+  semester: 5 
+ },
+
+ { 
+  name: "Subject 2", 
+  semester: 5
+ },
+
+ { 
+  name: "Subject 3", 
+  semester: 5 
+ },
+
+ { 
+  name: "Subject 4", 
+  semester: 5 
+ },
+
+ { 
+  name: "Subject 4", 
+  semester: 5 
+ },
+
+ //sem6
+ { 
+  name: "Subject 1", 
+  semester: 6 
+ },
+
+ { 
+  name: "Subject 2", 
+  semester: 6
+ },
+
+ { 
+  name: "Subject 3", 
+  semester: 6 
+ },
+
+ { 
+  name: "Subject 4", 
+  semester: 6 
+ },
+
+ { 
+  name: "Subject 4", 
+  semester: 6 
+ },
+];
+
+module.exports = subjects;
