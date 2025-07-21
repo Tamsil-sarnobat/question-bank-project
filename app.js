ae25a809ae0144b686b4118dd8d7815cbd02874d
tamsil sarnobat <tamsilsarnobat57@gmail.com>
2025-07-21 17:28:32 +0530

    Authentication & student contribution added

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const methodOverride = require("method-override");
 const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError");
 const {userSchema} = require("./schema.js");
 const wrapAsync = require("./utils/wrapAsync.js");
 const Subject = require("./models/subject.js");
-const cookieParser = require("cookie-parser");
 const session = require("express-session");
 const passport = require("passport");
 const  localStrategy = require("passport-local");
 const User = require("./models/users");
 const flash = require("connect-flash");
+const Question = require("./models/newQues.js");
 
 const mongoLink = "mongodb://127.0.0.1:27017/QBProject";
 
@@ -43,7 +44,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "/public")));
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser("mySecretCookie"));
+app.use(methodOverride('_method'))
 app.use(session(sessionOption));
 app.use(flash());
 app.use(passport.initialize());
@@ -56,7 +57,7 @@ passport.deserializeUser(User.deserializeUser());
 
 
 
-
+//Validate user
 const validateUser = (req,res,next)=>{
   let {error} = userSchema.validate(req.body);
   if(error){
@@ -66,16 +67,105 @@ const validateUser = (req,res,next)=>{
   }
 };
 
+
+
 //flash message
 app.use((req,res,next)=>{
   res.locals.error = req.flash("error");
   res.locals.success = req.flash("success");
+  res.locals.currUser = req.user;
   next();
 });
 
+
+
+//is Logged in middlewares
+const isLoggedIn = (req,res,next)=>{
+  if(!req.isAuthenticated()){
+      req.session.redirectUrl = req.originalUrl;
+      console.log(req.session.redirect);
+      req.flash("error","User Must Be Logged In!");
+      return res.redirect("/semester/login");
+  }
+  next();
+  
+}
+
+// route saving redirect middleware
+const saveReturnTo = (req,res,next)=>{
+  if (req.session.redirectUrl) {
+    res.locals.redirectUrl = req.session.redirectUrl;
+  }
+  next();
+}
+
+
+
+
+///homepage
 app.get("/", (req, res) => {
   res.render("semesters/home");
 });
+
+//display suggested question page
+app.get("/semester/newQuePage",wrapAsync(async (req,res)=>{
+  const question = await  Question.find({}).populate("username");
+  res.render("suggestedQuestion/suggestedQuesPage",{question});
+}))
+
+//Suggession Question form
+app.get("/semester/newQues",isLoggedIn,(req,res)=>{
+    res.render("suggestedQuestion/suggQuesForm");
+})
+
+//suggestion question post
+app.post("/semester/newQues",wrapAsync(async (req,res)=>{
+  let {subject,question} = req.body;
+
+  let newQuestion = new Question({
+    username:req.user._id,
+    subject:subject,
+    question:question
+  });
+  let ques = await newQuestion.save();
+  console.log(ques);
+  console.log(ques._id);
+  req.flash("success","You have contributed");
+  res.redirect("/semester/newQuePage");
+}))
+
+//suggested question edit get request
+
+app.get("/semester/newQues/:id/edit",isLoggedIn,wrapAsync(async (req,res)=>{
+  let {id} = req.params;
+  const question = await  Question.findById(id).populate("username");
+  res.render("suggestedQuestion/suggQuesEdit",{question});
+}))
+
+
+//suggested question edit PUT request
+
+app.put("/semester/newQues/:id",wrapAsync(async (req,res)=>{
+  let {id} = req.params;
+  let {subject,question}= req.body;
+  console.log(id);
+  const newQuestion = await  Question.findByIdAndUpdate(
+    id,
+    {subject,question},{runValidators:true,new:true}
+  );
+  console.log(newQuestion);
+ res.redirect("/semester/newQuePage");
+}))
+
+//suggested question edit PUT request
+
+app.delete("/semester/newQues/:id",wrapAsync(async(req,res)=>{
+  let {id} = req.params;
+  let deletedQues = await Question.findByIdAndDelete(id);
+  console.log(deletedQues);
+  res.redirect("/semester/newQuePage");
+}))
+
 
 
 //Sign-in route get/post
@@ -83,6 +173,7 @@ app.get("/semester/signin",(req,res)=>{
   res.render("users/signinForm");
 })
 
+//post
 app.post("/semester/signin",validateUser,wrapAsync(async (req,res)=>{
   try{
       let {username,email,password,role,semester} = req.body;
@@ -109,22 +200,35 @@ app.get("/semester/login",(req,res)=>{
 })
 
 
-app.post("/semester/login",passport.authenticate("local",{
+app.post("/semester/login",saveReturnTo,passport.authenticate("local",{
   failureRedirect:"/semester/login",
   failureFlash:true,
 }),wrapAsync(async (req,res)=>{
-   req.flash("success","welcom Back to regal College");
-   return res.redirect("/");
+  req.flash("success","welcom Back to regal College");
+  let redirectUrl = res.locals.redirectUrl || "/";
+  console.log(redirectUrl);
+  res.redirect(redirectUrl);
 }))
 
 
-app.get("/semesters/:id", async(req, res) => {
+//Semester 
+app.get("/semesters/:id", wrapAsync(async(req, res) => {
   const semId = parseInt(req.params.id);
   const Subjects = await Subject.find({semester: semId});
   res.render("semesters/semester",{ semId, Subjects });
+}));
+
+app.get("/logout",(req,res)=>{
+  req.logOut((err)=>{
+    if(err){
+      return next(err);
+    }
+    req.flash("success","You Logged Out!");
+    res.redirect("/");
+  });
 });
 
-
+//page  not found
 app.use((req, res, next) => {
   next(new ExpressError(404, "Page not found!"));
 });
